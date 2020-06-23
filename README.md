9c43332502771bfde7c340e9382eb86bd45f2689
JadinTredup <Jadin.Tredup@gmail.com>
2020-06-22 20:38:44 -0700

    Fixed SSVEP presentation issues and started the README.

---
+++ README.md
@@ -0,0 +1,20 @@
+# EEG Notebooks
+
+## Getting Started
+
+### Tutorials:
+***Coming Soon*** we will have the following tutorials available:
+
+* Installation instructions.
+* Setting up your OpenBCI Ultracortex Mark IV headset.
+* First time running the notebooks.
+* Adding an electrode to the Muse headband.
+
+## Available Experiments
+
+### N170
+
+### Visual P300
+
+### SSVEP
+Steady-State Visually Evoked Potentials (SSVEP) are...
--- run_notebooks.py
+++ run_notebooks.py
@@ -7,13 +7,13 @@ from eegnb.experiments.visual_ssvep import ssvep
 
 def generate_save_fn(board_name, experiment, subject_id):
     '''Generates a file name with the proper trial number for the current subject/experiment combo'''
-    data_dir = os.path.join('data', experiment)
+    data_dir = os.path.join('data', experiment, subject_id)
     if not os.path.exists(data_dir):
         os.mkdir(data_dir)
 
     # Check currently existing files to iterate to most recent trial number for the session/subject
     trial_num = 0
-    file_name = f"{subject_id}_TRIAL_{trial_num}_{board_name}.csv"
+    file_name = f"{board_name}_TRIAL_{trial_num}.csv"
     save_fp = os.path.join(data_dir, file_name)
     while os.path.exists(save_fp):
         trial_num += 1
