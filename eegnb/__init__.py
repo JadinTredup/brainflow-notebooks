bd8c35ded803bcf65bc2c7ce7128d7c958db5805
John Griffiths <j.davidgriffiths@gmail.com>
2020-07-13 00:08:46 -0400

    change make dirs command to allow recursive directory creation

--- eegnb/__init__.py
+++ eegnb/__init__.py
@@ -6,7 +6,7 @@ def generate_save_fn(board_name, experiment, subject_id):
     '''Generates a file name with the proper trial number for the current subject/experiment combo'''
     data_dir = os.path.join(DATA_DIR, experiment, subject_id)
     if not os.path.exists(data_dir):
-        os.mkdir(data_dir)
+        os.makedirs(data_dir)
 
     # Check currently existing files to iterate to most recent trial number for the session/subject
     trial_num = 0
